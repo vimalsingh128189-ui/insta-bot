f4862f945a25e637c1772baaa1b2fc760e4c06ee
vimalsingh128189-ui <vimalsingh128189@gmail.com>
2025-08-04 07:18:15 +0530

    Update Credentials.kt

--- src/main/kotlin/Credentials.kt
+++ src/main/kotlin/Credentials.kt
@@ -1,4 +1,5 @@
 object Credentials {
-    const val USERNAME = "enter_your_instagram_username_here"
-    const val PASSWORD = "enter_your_instagram_password_here"
+    const val USERNAME = "_r.vs._"
+    const val PASSWORD = "vimal@128189
+    "
 }
